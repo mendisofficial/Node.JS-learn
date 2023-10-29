92d4ae9813a155b336a7cca88c2fc62cdd47bd84
Chathusha Mendis <chathushamendis@icloud.com>
2023-10-29 11:35:50 +0530

    intro

---
+++ 1-intro.js
@@ -0,0 +1,25 @@
+/**
+ * Introduction to Node.js
+ *
+ * Node.js is a runtime environment that allows you to execute JavaScript on the server side.
+ * It's built on the V8 JavaScript engine and is known for its non-blocking, event-driven architecture.
+ * Node.js is widely used for web development, creating APIs, and building real-time applications.
+ *
+ * Key Points:
+ * - JavaScript on the server side.
+ * - Event-driven, non-blocking I/O.
+ * - Rich ecosystem of modules and packages.
+ * - Ideal for building scalable and high-performance applications.
+ *
+ * This is a simple Node.js program that outputs a message to the console.
+ */
+
+const amount = 8;
+
+if (amount < 10) {
+    console.log('small number');
+} else {
+    console.log('large number');
+}
+
+console.log(`hey it's my first node app!!!`);
---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.JS-learn
+
+Welcome to the "Node.JS-learn" repository! This is a hub for learning and mastering Node.js, a powerful JavaScript runtime for server-side applications.
